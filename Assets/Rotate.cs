8cf9379137db9143424e7593ebf4d23cfdb6b85a
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-19 16:35:23 +0300

    Start

---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+
+    }
+}
