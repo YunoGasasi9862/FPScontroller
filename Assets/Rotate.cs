d6dfadf58a7ca1d2d414b176529fa2049c16e49b
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-21 12:58:36 +0300

    ss

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -7,10 +7,11 @@ public class Rotate : MonoBehaviour
     // Start is called before the first frame update
     public float MouseInsensitivity = 200f;
     public Transform playerBody;
-    public float yRotation;
+    float yRotation = 0;
     void Start()
     {
-        playerBody = GetComponent<Transform>();
+       //I WAS TRYING TO CACHE THE TRANSFORM OF THE FUCKING CAMERA!!!
+      Cursor.lockState = CursorLockMode.Locked; //this locks the cursor ->cursor.locksate=cursorlockmade.locked!
     }
 
    
@@ -19,10 +20,11 @@ public class Rotate : MonoBehaviour
             float mouseX = Input.GetAxis("Mouse X") * MouseInsensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * MouseInsensitivity * Time.deltaTime;
 
-           yRotation = -mouseY; //why y rotation is -mousey? (inverted,thats why)
+           yRotation -= mouseY; //why y rotation is -mousey? (inverted,thats why)
                                 //here we ant to rotate the camera
-         Mathf.Clamp(yRotation, -90, 90);
-        transform.localRotation = transform.localEulerAngles();         
+                                //keep subtrating the mousey From the rotatation!!
+           yRotation= Mathf.Clamp(yRotation, -90f ,90f);
+           transform.localRotation = Quaternion.Euler(yRotation, 0,0);       
            playerBody.Rotate(Vector3.up * mouseX); 
         // it has to be mouse x * Vector3.up because you want to rotate it on X axis against Y
 
