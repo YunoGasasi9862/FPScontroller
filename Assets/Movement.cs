647d4018e6ede420f1bcde08674e6cf335938acd
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-23 16:09:11 +0300

    ss

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    float speed = 200f;
+    float speed = 20f;
     public CharacterController controller;
+    Vector3 velocity;
+    float gravity = -9.81f;
     void Start()
     {
         
@@ -17,10 +19,14 @@ public class Movement : MonoBehaviour
     {
         float X = Input.GetAxis("Horizontal");
         float Z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * X + transform.forward * Z; //(forward for Z)
+        // Vector3 move = transform.right * X + transform.forward * Z; //(forward for Z)
         //right for X
+        Vector3 move = transform.right * X + transform.forward * Z;
+        controller.Move(move * Time.deltaTime * speed);
 
-       
+
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
