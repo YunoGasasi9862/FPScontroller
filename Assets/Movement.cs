09ec4ba36c5ca56551d7e9d511ab48e17d5ccdb9
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-25 13:01:19 +0300

    ss

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -13,20 +13,21 @@ public class Movement : MonoBehaviour
    public Transform groundcheck;
     public float GroundDistance = 0.4f;
     public LayerMask groundmask;
-    void Start()
-    {
-        
-    }
+    public float jumpheight;
+   
 
     // Update is called once per frame
     void Update()
     {
+
         isgrounded = Physics.CheckSphere(groundcheck.position, GroundDistance, groundmask);
 
-        if (isgrounded && velocity.y < 0)
+
+        if(isgrounded && velocity.y <0)
         {
             velocity.y = -2f;
         }
+
         float X = Input.GetAxis("Horizontal");
         float Z = Input.GetAxis("Vertical");
         // Vector3 move = transform.right * X + transform.forward * Z; //(forward for Z)
@@ -34,7 +35,10 @@ public class Movement : MonoBehaviour
         Vector3 move = transform.right * X + transform.forward * Z;
         controller.Move(move * Time.deltaTime * speed);
 
-       
+       if(Input.GetKey(KeyCode.Space) && isgrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
 
         velocity.y += gravity;
         controller.Move(velocity * Time.deltaTime);
